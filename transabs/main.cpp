b935471683260d6315adcf2fd008b7f549a1e567
Ionut Georgescu <ig@dhcp236.mpipks-dresden.mpg.de>
2011-08-24 18:32:06 +0200

    Initial Commit

---
+++ transabs/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  transabs
+//
+//  Created by Ionuț Georgescu on 8/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
