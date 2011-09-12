8dc50dfc3c9835b7eb7322a2c8f036aa2f3bae1d
Ionuț Georgescu <ionutg@gmail.com>
2011-09-12 12:14:03 +0200

    First commit after a log time. It just works.

--- src/main.cpp
+++ src/main.cpp
@@ -11,190 +11,271 @@
 #include <cstdlib>
 #include <cmath>
 
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/assign/list_inserter.hpp>
+
 #include "Common.h"
 #include "Argon.h"
 #include "Laser.h"
 #include "DirectSum.h"
-#include "Utils.h"
+#include "HDF5IO.h"
+#include "Electron.h"
+#include "Localization.h"
 
 using namespace std;
 
+namespace fs = boost::filesystem;
+
+using namespace boost::assign;
+
 po::variables_map vm;
 
-int Natom, Nparticles, NMaxParticles;
-vec    q, mass, invmass, x, y, z, vx, vy, vz, fx, fy, fz, phi;
+int Natom, Nparticles;
+vec    q, mass, x, y, z, vx, vy, vz, fx, fy, fz, phi, next_atom_dist,
+new_next_atom_dist, revangle, next_atom_t0;
+ivec next_atom, new_next_atom, nloc;
 double  Utot;
+
+VectorMap<double>   fDumpVectors, fVectors;
+VectorMap<int>  iVectors;
+
 
 Argon   sp;
 
-double t, tstart, tstop, dt;
-double soft_core;
+double t, tstart, tstop, dt, dumpdt = 10.0;
+double soft_core, LocalizationAngle = 3.0;
 
 GaussianLaserPulse pump;
 
 
-void process_options ( int argc, char * argv[] )
+void process_options (int argc, char * argv[])
 {
-    try {
+    try
+    {
 
-        po::options_description cmdline ( "Command line options" );
+        po::options_description cmdline ("Command line options");
         cmdline.add_options()
-        ( "help", "get help" )
-        ( "config", po::value<string>(), "configuration file" );
+        ("help", "get help")
+        ("config", po::value<string>(), "configuration file");
 
-        po::options_description generic ( "General options" );
+        po::options_description generic ("General options");
         generic.add_options()
-        ( "pump.wave-length", po::value<double>()->required(), "wave length of the pump laser [nm]" )
-        ( "pump.intensity", po::value<double>()->required(), "intensity of the pump laser [W/cm^2]" )
-        ( "pump.fwhm", po::value<double>()->required(), "pulse length of the pump laser [fs]" )
-        ( "coordinates", po::value<string>()->required(), "initial coordinates of the cluster atoms" )
-        ( "eps", po::value<double>()->required(), "soft-core parameter" )
-        ( "tstart", po::value<double>()->required(), "soft-core parameter" )
-        ( "tstop", po::value<double>()->required(), "soft-core parameter" )
-        ( "dt", po::value<double>()->required(), "soft-core parameter" )
+        ("pump.wave-length", po::value<double>()->required(), "wave length of the pump laser [nm]")
+        ("pump.intensity", po::value<double>()->required(), "intensity of the pump laser [W/cm^2]")
+        ("pump.fwhm", po::value<double>()->required(), "pulse length of the pump laser [fs]")
+        ("coordinates", po::value<string>()->required(), "initial coordinates of the cluster atoms")
+        ("eps", po::value<double>()->required(), "soft-core parameter")
+        ("tstart", po::value<double>()->required(), "soft-core parameter")
+        ("tstop", po::value<double>()->required(), "soft-core parameter")
+        ("dt", po::value<double>()->required(), "soft-core parameter")
+        ("dump.interval", po::value<double>()->required(), "time interval for data dump")
+        ("dump.file", po::value<string>()->required(), "dump data to this HDF5 file")
+        ("localization_angle", po::value<double>()->required(), "localization angle in multiples of pi")
         ;
 
-        cmdline.add ( generic );
+        cmdline.add (generic);
 
         po::positional_options_description  p;
-        p.add ( "coordinates", 1 );
+        p.add ("coordinates", 1);
 
-        po::store ( po::command_line_parser ( argc, argv ).options ( cmdline ).positional ( p ).run(), vm );
+        po::store (po::command_line_parser (argc, argv).options (cmdline).positional (p).run(), vm);
 
-        if ( vm.count ( "config" ) ) {
-            ifstream cfgin ( vm["config"].as<string>().c_str() );
-            po::store ( po::parse_config_file ( cfgin, generic ), vm );
+        if (vm.count ("config"))
+        {
+            ifstream cfgin (vm["config"].as<string>().c_str());
+            po::store (po::parse_config_file (cfgin, generic), vm);
             cfgin.close();
         }
 
-        po::notify ( vm );
+        po::notify (vm);
 
-        if ( vm.count ( "help" ) ) {
+        if (vm.count ("help"))
+        {
             cout << "\n\t\tAsk Ionut!\n\n" << cmdline << endl;
-            exit ( EXIT_FAILURE );
+            exit (EXIT_FAILURE);
         }
     }
-    catch ( po::required_option& ro ) {
+    catch (po::required_option& ro)
+    {
         cerr << "Required option " << ro.get_option_name() << " is missing.\n";
-        exit ( EXIT_FAILURE );
+        exit (EXIT_FAILURE);
     }
-    catch ( exception& e ) {
+    catch (exception& e)
+    {
         cerr << "error: " << e.what() << "\n";
-        exit ( EXIT_FAILURE );
+        exit (EXIT_FAILURE);
     }
+
     soft_core = vm["eps"].as<double>();
+
     tstart = vm["tstart"].as<double>();
     tstop = vm["tstop"].as<double>();
     dt = vm["dt"].as<double>();
 
     // I_au = c eps_0 E_au^2/2
     // w_au = 2*pi*\hbar*c/E_H
-    pump = GaussianLaserPulse ( vm["pump.intensity"].as<double>() / 3.50944493695457e+16,
-                45.5633525101396 / vm["pump.wave-length"].as<double>(),
-                vm["pump.fwhm"].as<double>() * 41.3413733524035 );
-    pump.setZero ( 2.5 * vm["pump.fwhm"].as<double>() * 41.3413733524035 );
+    pump = GaussianLaserPulse (vm["pump.intensity"].as<double>() / 3.50944493695457e+16,
+                               45.5633525101396 / vm["pump.wave-length"].as<double>(),
+                               vm["pump.fwhm"].as<double>() * 41.3413733524035);
+    pump.setZero (2.5 * vm["pump.fwhm"].as<double>() * 41.3413733524035);
+
+    dumpdt = vm["dump.interval"].as<double>();
+    LocalizationAngle = vm["localization_angle"].as<double>() * M_PI;
+
 }
 
-void DumpSnapshot(int   i)
-{
-    stringstream fn;
-}
 
 double kinetic_energy()
 {
-    vec ret ( 1 );
+    vec ret (1);
 
-    ret.rows ( 0, 0 ) =  0.5 * sum (
-                             ( square(vx.rows ( 0, Nparticles - 1 ) )
-                               + square(vy.rows ( 0, Nparticles - 1 ))
-                               + square(vz.rows ( 0, Nparticles - 1 )) ) % mass.rows ( 0, Nparticles - 1 ) ) ;
+    ret.rows (0, 0) =  0.5 * sum ( (square (vx) + square (vy) + square (vz)) % mass);
 
-    return ret ( 0 );
+    return ret (0);
+}
+
+template<typename T>
+void init_fields (VectorMap<T>& f, int N)
+{
+    typename VectorMap<T>::iterator j;
+
+    for (j = f.begin(); j != f.end(); j++)
+    {
+        j->second->zeros (N);
+    }
 }
 
 
-int main ( int argc, char * argv[] )
+void resize_vectors ()
+{
+    VectorMap<double>::iterator jf;
+    VectorMap<int>::iterator ji;
+    int	N;
+    
+    N = 1.5*fVectors.begin()->second->n_rows;
+
+    for (jf = fVectors.begin(); jf != fVectors.end(); jf++)
+    {
+        jf->second->reshape (N, 1);
+    }
+    
+    for (ji = iVectors.begin(); ji != iVectors.end(); ji++)
+    {
+        ji->second->reshape (N, 1);
+    }
+}
+
+
+
+int main (int argc, char * argv[])
 {
     mat rt;
-    int ndt;
+    int ndt, ndump, NMaxParticles;
 
-    process_options ( argc, argv );
+    process_options (argc, argv);
 
-    rt.load ( vm["coordinates"].as<string>() );
+    rt.load (vm["coordinates"].as<string>());
     rt *= 3.84 * 1.89;
     Natom = rt.n_rows;
     NMaxParticles = 5 * Natom;
 
-    x.zeros ( NMaxParticles );
-    y = x;
-    z = x;
-    vx = x;
-    vy = x;
-    vz = x;
-    fx = x;
-    fy = x;
-    fz = x;
-    phi = x;
-    q = x;
-    mass = x;
-    invmass = x;
-
-    x.rows ( 0, Natom - 1 ) = rt.col ( 0 );
-    y.rows ( 0, Natom - 1 ) = rt.col ( 1 );
-    z.rows ( 0, Natom - 1 ) = rt.col ( 2 );
-
-    q.rows ( 0, Natom - 1 ).fill ( 1.0 );
-    mass.rows ( 0, Natom - 1 ).fill ( sp.getMass() );
 
 
+    insert (fDumpVectors) ("q",  &q) ("mass", &mass)
+        ("x", &x) ("y", &y) ("z", &z)
+        ("vx", &vx) ("vy", &vy) ("vz", &vz)
+        ("fx", &fx) ("fy", &fy) ("fz", &fz)
+        ("phi", &phi);
+
+    fVectors = fDumpVectors;
+    insert (fVectors) ("next_atom_dist", &next_atom_dist)
+        ("new_next_atom_dist", &new_next_atom_dist) ("revangle", &revangle);
+
+    insert (iVectors) ("next_atom", &next_atom)
+        ("new_next_atom", &new_next_atom);
+
+    init_fields (fVectors, NMaxParticles);
+    init_fields (iVectors, NMaxParticles);
+
+    q.zeros();
+    mass.ones();
+    next_atom.fill (-2);
+    nloc.zeros (Natom);
+
+    x.rows (0, Natom - 1) = rt.col (0);
+    y.rows (0, Natom - 1) = rt.col (1);
+    z.rows (0, Natom - 1) = rt.col (2);
     rt.reset();
+    
+    Nparticles = Natom;
 
-    q.rows ( Natom, 2*Natom - 1 ).fill ( -1.0 );
-    mass.rows ( Natom, 2*Natom - 1 ).fill ( 1.0 );
-    x.rows ( Natom, 2*Natom - 1 ) = x.rows ( 0, Natom - 1 );
-    y.rows ( Natom, 2*Natom - 1 ) = y.rows ( 0, Natom - 1 );
-    z.rows ( Natom, 2*Natom - 1 ) = z.rows ( 0, Natom - 1 );
 
-    Nparticles = 2 * Natom;
+    mass.rows (0, Natom - 1).fill (sp.getMass());
 
-    double pol[3];
-    for ( int i = 0; i < Natom; i++ ) {
-        pol [0] = sqrt ( 2.0 * ( 1.0 / soft_core - sp.Eip ( 0 ) ) );
-        pol [1] = acos ( 2.0 * myrand() - 1.0 );
-        pol [2] = 2.0 * M_PI * myrand();
-        polar2cart ( pol, vx ( Natom + i ), vy ( Natom + i ), vz ( Natom + i ) );
-
+    for (int i = 0; i < Natom; i++)
+    {
+        create_electron(&sp, i);
     }
 
-    invmass.rows ( 0, Nparticles - 1 ) = 1.0 / mass.rows ( 0, Nparticles - 1 );
-
     double Ekin;
-    ofstream eout ( "energy.dat" );
 
-    DirectSum ();
+    ofstream eout ("energy.dat");
+
+    DirectSum();
     Ekin = kinetic_energy();
     eout << t << "\t" << Utot << "\t" << Ekin << "\t" << Utot + Ekin << endl;
 
-    ndt = ( tstop - tstart ) / dt;
-    for ( int i = 1; i <= ndt; i++, t = tstart + dt * i ) {
-        vx.rows ( 0, Nparticles - 1 ) += ( 0.5 * dt ) * fx.rows ( 0, Nparticles - 1 ) % q.rows ( 0, Nparticles - 1 ) % invmass.rows ( 0, Nparticles - 1 );
-        vy.rows ( 0, Nparticles - 1 ) += ( 0.5 * dt ) * fy.rows ( 0, Nparticles - 1 ) % q.rows ( 0, Nparticles - 1 ) % invmass.rows ( 0, Nparticles - 1 );
-        vz.rows ( 0, Nparticles - 1 ) += ( 0.5 * dt ) * ( fz.rows ( 0, Nparticles - 1 ) + pump.field ( t ) ) % q.rows ( 0, Nparticles - 1 ) % invmass.rows ( 0, Nparticles - 1 );
+    ndt = (tstop - tstart) / dt;
+    ndump = dumpdt / dt;
+    HDF5IO h5dump (vm["dump.file"].as<string>());
 
-        x.rows ( 0, Nparticles - 1 ) += dt * vx.rows ( 0, Nparticles - 1 );
-        y.rows ( 0, Nparticles - 1 ) += dt * vy.rows ( 0, Nparticles - 1 );
-        z.rows ( 0, Nparticles - 1 ) += dt * vz.rows ( 0, Nparticles - 1 );
+    for (int i = 1; i <= ndt; i++, t = tstart + dt * i)
+    {
+        vec invmass = 1.0 / mass;
 
-        DirectSum ();
+        vx += (0.5 * dt) * fx % q % invmass;
+        vy += (0.5 * dt) * fy % q % invmass;
+        vz += (0.5 * dt) * (fz + pump.field (t)) % q % invmass;
+
+        x += dt * vx;
+        y += dt * vy;
+        z += dt * vz;
+
+        DirectSum();
 
 
-        vx.rows ( 0, Nparticles - 1 ) += ( 0.5 * dt ) * fx.rows ( 0, Nparticles - 1 ) % q.rows ( 0, Nparticles - 1 ) % invmass.rows ( 0, Nparticles - 1 );
-        vy.rows ( 0, Nparticles - 1 ) += ( 0.5 * dt ) * fy.rows ( 0, Nparticles - 1 ) % q.rows ( 0, Nparticles - 1 ) % invmass.rows ( 0, Nparticles - 1 );
-        vz.rows ( 0, Nparticles - 1 ) += ( 0.5 * dt ) * ( fz.rows ( 0, Nparticles - 1 ) + pump.field ( t ) ) % q.rows ( 0, Nparticles - 1 ) % invmass.rows ( 0, Nparticles - 1 );
+        vx += (0.5 * dt) * fx % q % invmass;
+        vy += (0.5 * dt) * fy % q % invmass;
+        vz += (0.5 * dt) * (fz + pump.field (t)) % q % invmass;
+	
+        Localization(dt);
 
-        Ekin = kinetic_energy();
-        eout << t << "\t"<< pump.field(t)<<"\t"<< Utot << "\t" << Ekin << "\t" << Utot + Ekin << endl;
+        for (int j = 0; j < Natom; j++)
+        {
+            if (nloc[j] == 0)
+            {
+                create_electron (&sp, j);
+            }
+        }
+
+        if (i % 10 == 0)
+        {
+            Ekin = kinetic_energy();
+            eout << t << "\t" << pump.field (t) << "\t" << Utot << "\t" << Ekin << "\t" << Utot + Ekin << endl;
+        }
+
+        if (i % ndump == 0)
+        {
+            h5dump.newGroup();
+            h5dump.add (fDumpVectors, Nparticles);
+            h5dump.add ("nloc", nloc, Natom);
+            h5dump.add ("next_atom", next_atom, Nparticles - Natom);
+            h5dump.add ("revangle", revangle, Nparticles - Natom);
+            h5dump.closeGroup();
+        }
     }
+
     eout.close();
 
     return 0;
